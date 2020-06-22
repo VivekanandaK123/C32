87fb546767c4580c6783d1f8f936ac50f971a5ea
VivekanandaK123 <vivekanandakatakam16@gmail.com>
2020-06-22 15:02:51 +0530

    bg

--- sketch.js
+++ sketch.js
@@ -7,11 +7,13 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var bg = "sprites/bg.png";
+var score = 0;
 
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBgImage();
 }
 
 function setup(){
@@ -45,18 +47,26 @@ function setup(){
 }
 
 function draw(){
+   if(backgroundImg){
     background(backgroundImg);
+   } 
+ fill("white");
+  text("Score : "+score,900,50);
+
     Engine.update(engine);
+
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score()
     log3.display();
 
     box5.display();
@@ -67,6 +77,8 @@ function draw(){
     platform.display();
     //log6.display();
     slingshot.display();    
+
+
 }
 
 function mouseDragged(){
@@ -85,4 +97,21 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getBgImage(){
+     var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo");
+     var responseJSON = await response.json();
+     var dateTime = responseJSON.datetime;
+     var hour = dateTime.slice(11,13);
+
+     if(hour >= 06 && hour <= 17){
+     bg = "sprites/bg.png";
+     }
+     else{
+         bg = "sprites/bg2.jpg"
+     }
+     backgroundImg = loadImage(bg);
+     console.log(backgroundImg);
+
 }
